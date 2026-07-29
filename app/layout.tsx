f89db10382c8d3110bca44af9ef2a7f9b8049412
Den Mokrinsky <mokrinsky.denis@gmail.com>
2026-07-29 11:56:33 +0300

    Add Yandex.Metrika, Dockerfile, docker-compose, standalone output

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { LanguageProvider } from "@/components/language-context";
 
@@ -29,7 +30,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={inter.variable}>
+      <head>
+        <Script id="yandex-metrika" strategy="afterInteractive">
+          {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};m[i].l=1*new Date();for(var j=0;j<document.scripts.length;j++){if(document.scripts[j].src===r){return;}}k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})(window,document,'script','https://mc.yandex.ru/metrika/tag.js','ym');ym(68126902,'init',{webvisor:true,clickmap:true,referrer:document.referrer,url:location.href,accurateTrackBounce:true,trackLinks:true});`}
+        </Script>
+      </head>
       <body className="min-h-screen antialiased">
+        <noscript>
+          <img src="https://mc.yandex.ru/watch/68126902" style={{ position: "absolute", left: "-9999px" }} alt="" />
+        </noscript>
         <LanguageProvider>{children}</LanguageProvider>
       </body>
     </html>
